a5ba9338ba1c4b9b576dbc33d17067966e9a4d05
JhonnyRamos04 <onlyjhonny01@gmail.com>
2024-10-24 22:08:28 -0400

    has been add all img and change HeroSection

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,13 +1,14 @@
 import { HeroLink } from "./ui/HeroLink"
+import { Wave } from "./ui/Wave"
 
 export const Hero = () => {
     return (
-        <section className='bg-gray-100 py-16 lg:py-20 flex items-center justify-center'>
+        <section className='flex justify-between'>
             <div
-                className='flex flex-col p-4 md:p-0 lg:flex-row items-center justify-around w-full lg:mt-2'
+                className='bg-[#6595F9] h-screen flex flex-col px-20 py-4 items-center justify-center'
             >
-                <div className='max-w-[600px]'>
-                    <h1 className='text-4xl md:text-5xl font-bold text-gray-900 mb-4'>
+                <div className="max-w-[600px]">
+                    <h1 className='text-4xl md:text-5xl font-bold text-gray-800 mb-4'>
                         ¡Desafía tus límites en las alturas!
                     </h1>
                     <p className='text-base md:text-xl text-gray-700 mb-6 text-pretty'>
@@ -15,16 +16,16 @@ export const Hero = () => {
                         disfruta de vistas panorámicas y siente la libertad de moverte con
                         gracia y precisión. ¡Te garantizamos una experiencia única!
                     </p>
-                    <HeroLink>Inscribite Ya</HeroLink>
                 </div>
-                <div>
-                    <img
-                        src='/Climp-img.png'
-                        alt='Hero image'
-                        className='size-[350px] md:size-[550px]'
-                    />
+                <HeroLink>Inscribite Ya</HeroLink>
+                <div className="bg-gray-100/40 w-screen backdrop-blur-sm h-screen absolute -z-10 right-0 top-0"></div>
+                <div className="w-[700px] object-cover absolute top-0 right-0 -z-20">
+                    <img src="/HeroClimb2.jpg" alt="Climbing-IMG" className=" w-full h-screen " />
                 </div>
+
             </div>
+            <Wave className="rotate-90 h-[200px] absolute bottom-[350px] right-36 z-0 " />
+
         </section>
     )
 }
--- src/components/ui/HeroLink.jsx
+++ src/components/ui/HeroLink.jsx
@@ -1,4 +1,4 @@
-export const HeroLink = ({ promp }) => {
+export const HeroLink = ({ children }) => {
     return (
         <>
             <div className='flex items-center justify-center'>
@@ -13,7 +13,7 @@ export const HeroLink = ({ promp }) => {
                         <span className='relative z-10 block px-6 py-3 rounded-xl bg-sky-800'>
                             <div className='relative z-10 flex items-center space-x-2'>
                                 <span className='transition-all duration-500 group-hover:translate-x-1'
-                                >{promp}</span>
+                                >{children}</span>
                                 <svg
                                     className='w-6 h-6 transition-transform duration-500 group-hover:translate-x-1'
                                     data-slot='icon'
